46df42e0fcf28889e2bfadc1f357919e6728efdc
iamkunalgoswami <imakshat.goswami@gmail.com>
2024-11-30 23:58:39 +0530

    datatypes

---
+++ 01_basic/03_datatypes.js
@@ -0,0 +1,20 @@
+"use-strict" //treat all js code as newer version
+
+// alert("Hello"); //we are nodejs and not Browser
+
+/**
+ * Datatypes
+ * number       2 to the power 52
+ * string       ""
+ * boolean      true/false
+ * null         standalone_value representation of empty value
+ * undefined    not at all defined
+ * symbol       unique
+ * 
+ * object       
+ */
+
+
+console.log(typeof "K");
+console.log(typeof null);
+console.log(typeof undefined);
